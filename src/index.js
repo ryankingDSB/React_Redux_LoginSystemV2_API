91f087b384721814088a1305b923c38c07e5783e
Ryan King <ryan.king@deepskyblue.com>
2018-01-25 10:10:17 +0000

    User Auth working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
+import auth from './routes/auth';
+
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.connect(process.env.MONGODB_URL);
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid credentials" }});
-})
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
